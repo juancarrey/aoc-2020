bb3cb5093a327fc57c4087483c96a56893f3316c
Juan Carrey Labarta <juankarrey@gmail.com>
2020-12-01 22:36:03 +0100

    Problem 1:2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use crate::problem_1::phase_1::solve_problem_1_phase_1;
+use crate::problem_1::phase_1::{solve_problem_1_phase_1, solve_problem_1_phase_2};
 
 mod problem_1;
 
 fn main() {
     solve_problem_1_phase_1();
+    solve_problem_1_phase_2();
+
 }
--- src/problem_1/phase_1.rs
+++ src/problem_1/phase_1.rs
@@ -4,16 +4,36 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashSet;
 
-pub fn solve_problem_1_phase_1() {
+pub fn solve_problem_1_phase_1() -> i32 {
     let input = read_input();
 
     for num in input.iter() {
         let remaining = 2020 - num;
         if input.contains(&remaining) {
-            println!("Result is {} * {} = {}", num, remaining, num * remaining);
-            break;
+            println!("Result (phase1) is {} * {} = {}", num, remaining, num * remaining);
+            return num * remaining;
         }
     }
+
+    return -1;
+}
+
+pub fn solve_problem_1_phase_2() -> i32 {
+    let input = read_input();
+
+    for num1 in input.iter() {
+        for num2 in input.iter() {
+            if num1 != num2 {
+                let remaining = 2020 - num1 - num2;
+                if input.contains(&remaining) {
+                    println!("Result (phase2) is {} * {} * {} = {}", num1, num2, remaining, num1 * num2 * remaining);
+                    return num1 * num2 * remaining;
+                }
+            }
+        }
+    }
+
+    return -1;
 }
 
 fn read_input() -> HashSet<i32> {
