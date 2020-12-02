a52722aa4df081512657eb9deeb0173feeb1b324
Juan Carrey Labarta <juankarrey@gmail.com>
2020-12-02 22:06:20 +0100

    Day 2 Phase 2

--- src/day_2/day_2.rs
+++ src/day_2/day_2.rs
@@ -4,8 +4,8 @@ use std::io::prelude::*;
 use std::fs::File;
 
 struct PasswordPolicy {
-    min: i32,
-    max: i32,
+    min: usize,
+    max: usize,
     char: char,
 }
 
@@ -13,25 +13,39 @@ impl PasswordPolicy {
     fn parse(policy_def: &str) -> PasswordPolicy {
         let range: Vec<&str> = policy_def[..policy_def.chars().count() - 2].split('-').collect();
         PasswordPolicy {
-            min: range[0].trim().parse::<i32>().unwrap(),
-            max: range[1].trim().parse::<i32>().unwrap(),
+            min: range[0].trim().parse::<usize>().unwrap(),
+            max: range[1].trim().parse::<usize>().unwrap(),
             char: policy_def.chars().last().unwrap()
         }
     }
 
-    fn validate(self, password: &str) -> bool {
-        let count: i32 = password.chars().map(|char| {
+    fn validate_phase_1(self, password: &str) -> bool {
+        return within_range(password.chars().map(|char| {
             return if char == self.char {
                 1
             } else {
                 0
             }
-        }).sum();
-        return count >= self.min && count <= self.max;
+        }).sum(), self.min, self.max);
+    }
+
+    fn validate_phase_2(self, password: &str) -> bool {
+        let expected = Some(self.char);
+        return xor(
+            password.chars().nth(self.min - 1) == expected,
+            password.chars().nth(self.max - 1) == expected);
     }
 }
 
-pub fn solve_day_2_phase_1() -> i32 {
+fn within_range(val: usize, min: usize, max: usize) -> bool {
+    return val >= min && val <= max;
+}
+
+fn xor(a: bool, b: bool) -> bool {
+    return (a || b) && !(a && b);
+}
+
+pub fn solve_day_2_phase_1() -> usize {
     let path = Path::new("src/day_2/day_2_input.txt");
     let file = BufReader::new(File::open(&path).unwrap());
 
@@ -43,7 +57,7 @@ pub fn solve_day_2_phase_1() -> i32 {
             let password = parsed_line[1].trim();
 
             let policy = PasswordPolicy::parse(policy_def);
-            return if policy.validate(password) {
+            return if policy.validate_phase_2(password) {
                 1
             } else {
                 0
